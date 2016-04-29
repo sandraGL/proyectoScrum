ce33e007efbbb1aff73fab82f1cbc9369d03f25b
eduardo ruiz <erosonic1313@gmail.com>
2016-04-29 16:52:29 +0200

    formularioSprint.php creado

---
+++ vista/formularioSprint.php
@@ -0,0 +1,30 @@
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Crear nuevo Sprint</title>
+		<meta charset="utf-8"/>
+                <link rel="stylesheet" type="text/css" href="estilo-formulario.css">
+	</head>
+	<body>
+		<form class="contact_form">
+			<ul>
+			<li>
+	        	<h2>Nuevo Sprint</h2>
+	         	
+		    </li>
+		   
+		  
+			
+			<li>
+			    <label for="message">Descripción:</label>
+			    <textarea name="message" cols="40" rows="6" ></textarea>
+			</li>
+			<li>
+			    <button class="submit" type="submit">Enviar</button>
+			</li>
+			</ul>
+		</form>
+	</body>
+</html>
